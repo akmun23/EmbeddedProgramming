bf205d36d9fa881b83f69497c902d4cbc5cfb6dd
Aksel Munksgaard <159116437+akmun23@users.noreply.github.com>
2025-02-23 13:04:17 +0100

    Shortened the main

--- Assigment2/main.c
+++ Assigment2/main.c
@@ -71,34 +71,16 @@ void select_button(int* currenTimer);
  */
 void buttonClick();
 
+void init_gpio(void);
+
 extern int ticks;
 
 int main(void){
 
    // Initialize sysTick interrupt
    init_systick();
+   init_gpio();
 
-   // Start variables
-   int dummy;
-   volatile int hasChanged = 0;
-
-   // Enable the GPIO port that is used for the on-board LEDs and switches
-   SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
-
-   // Do a dummy read to insert a few cycles after enabling the peripheral
-   dummy = SYSCTL_RCGC2_R;
-
-   // allow changes to PF4-0
-   GPIO_PORTF_CR_R = 0x1E;
-
-   // Set the direction as output (PF1 - PF3)
-   GPIO_PORTF_DIR_R = 0x0E;
-
-   // Enable the GPIO pins for digital function (PF1 - PF4)
-   GPIO_PORTF_DEN_R = 0x1E;
-
-   // Enable internal pull-up (PF4)
-   GPIO_PORTF_PUR_R = 0x1E;
 
    GPIO_PORTF_DATA_R |= (YELLOW+GREEN);
    // Loop forever
@@ -240,3 +222,26 @@ void select_button(int* currentTimer)
   }
 }
 
+void init_gpio(void){
+    // Start variables
+    int dummy;
+    volatile int hasChanged = 0;
+
+    // Enable the GPIO port that is used for the on-board LEDs and switches
+    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
+
+    // Do a dummy read to insert a few cycles after enabling the peripheral
+    dummy = SYSCTL_RCGC2_R;
+
+    // allow changes to PF4-0
+    GPIO_PORTF_CR_R = 0x1E;
+
+    // Set the direction as output (PF1 - PF3)
+    GPIO_PORTF_DIR_R = 0x0E;
+
+    // Enable the GPIO pins for digital function (PF1 - PF4)
+    GPIO_PORTF_DEN_R = 0x1E;
+
+    // Enable internal pull-up (PF4)
+    GPIO_PORTF_PUR_R = 0x1E;
+}
