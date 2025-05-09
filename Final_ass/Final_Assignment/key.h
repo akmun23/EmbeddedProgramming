23d897b6b776f80af50993f092d8d95d0b6e0380
JonathanDalsgaard <157690382+RosaalbaL1@users.noreply.github.com>
2025-05-09 15:41:51 +0200

    Skippidi elevator keypad password
    Skippidi

---
+++ Final_ass/Final_Assignment/key.h
@@ -0,0 +1,27 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: key.h
+*
+* PROJECT....: EMP
+*
+* DESCRIPTION: Test.
+*
+* Change Log:
+******************************************************************************
+* Date    Id    Change
+* YYMMDD
+* --------------------
+* 150321  MoH   Module created.
+*
+*****************************************************************************/
+
+#ifndef _KEY_H
+  #define _KEY_H
+
+INT8U get_keyboard();
+extern void key_task(void *pvParameters);
+
+
+#endif
